d3f66f5bb83011b7d773e72da428b5910df49138
Ian-c-cameron <ian.c.cam@gmail.com>
2020-10-15 03:39:50 +0000

    fix code injection vulnerability

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,13 +7,17 @@ const pool = new Pool({
   host: 'localhost',
   database: 'bootcampx'
 });
-
-pool.query(`
+const queryString = `
 SELECT students.id, students.name AS name, cohorts.name AS cohort_name
 FROM students JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${args[0]}%'
-LIMIT ${args[1]};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`;
+let cohortName = args[0];
+let limit = args[1];
+let values = [`%${cohortName}%`, limit];
+
+pool.query(queryString, values)
   .then(res => {
     console.log(res);
   })
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,15 +7,17 @@ const pool = new Pool({
   host: 'localhost',
   database: 'bootcampx'
 });
-
-pool.query(`
+let values = [`${args[0]}`];
+let queryString = `
 SELECT DISTINCT(teachers.name) AS teacher, cohorts.name AS cohort
 FROM teachers JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name = '${args[0]}'
+WHERE cohorts.name = $1
 ORDER BY teachers.name;
-`)
+`;
+
+pool.query(queryString, values)
   .then(res => {
     let rows = res.rows;
     for (const row of rows) {
